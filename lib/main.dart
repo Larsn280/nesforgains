859361b23fd512a2b1bef8caa61a70f8d6c3df67
Larsn280 <larsn280@hotmail.com>
2024-11-26 12:12:02 +0100

    AddedENVFileAndAWSWorking

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:nesforgains/app.dart';
 import 'package:nesforgains/database.dart/database.dart';
 import 'package:nesforgains/logger.dart';
+import 'package:nesforgains/service/aws_bucket_service.dart';
 
 void main() async {
   try {
     WidgetsFlutterBinding.ensureInitialized();
+
+    await dotenv.load();
+
     final sqflite = await setupSQLite();
+
+    final awsBucketService = AwsBucketService();
+    await awsBucketService.listBucketContents();
 
     runApp(App(
       sqflite: sqflite,
